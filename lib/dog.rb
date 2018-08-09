be7bb80a33fbb7a0373ddc8cd1e44c9f62ee1d2b
Sarah Ng <agbtr101a@gmail.com>
2018-08-09 19:48:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,19 @@
 # dog.rb
 class Dog
  def initialize 
- 
+@fido = fido
+@snoopy = snoopy
+@lassie = lassie
+end
+
+def color
+  @color
+end
+
+
+def color= (new_color)
+  @color = new_color
+end
+end
+
      
